9065cb4aa99f5cb34ceba7130a6b4c2f26411515
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2020-10-07 08:43:50 -0600

    Update rich.java

--- week04/HelloFromCharles/src/rich.java
+++ week04/HelloFromCharles/src/rich.java
@@ -4,5 +4,7 @@ public class rich {
 class HelloFromCharles {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Charles");
+        System.out.println("Hello, Charles. My name is Ethan");
+
     }
 }
