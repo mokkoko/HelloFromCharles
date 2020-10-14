47a5a85be7003e4a47fd441eaab2a0a0462a7dbe
williamstyler6 <wil15087@byui.edu>
2020-10-13 19:11:54 -0600

    Hello from Tyler

--- week04/HelloFromCharles/src/rich.java
+++ week04/HelloFromCharles/src/rich.java
@@ -5,6 +5,7 @@ class HelloFromCharles {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Charles");
         System.out.println("Hello, Charles. My name is Ethan");
+        System.out.println("Hello Charles, this is Tyler")
 
     }
 }
